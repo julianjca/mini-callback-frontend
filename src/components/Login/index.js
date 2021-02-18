e21777f761b3dc1f72c320300353be91e76ac700
Julian Anderson <julian@jumpcut.com>
2021-02-18 20:04:54 +0700

    sign in integration

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,7 +1,20 @@
-import React from 'react'
-import { Box, Center, Container, Heading, Stack, Input, FormControl, FormLabel, Button } from "@chakra-ui/react"
+import React, { useState } from 'react'
+import axios from 'axios'
+
+import { Center, Container, Heading, Stack, Input, FormControl, FormLabel, Button } from "@chakra-ui/react"
 
 const LogIn = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const submitForm = async () => {
+    const res = await axios.post(`http://localhost:3030/users/login`, {
+      email,
+      password,
+    })
+    console.log('logged in!')
+  }
+
   return (
     <Container>
       <Center>
@@ -13,13 +26,29 @@ const LogIn = () => {
         <Stack spacing={5}>
           <FormControl id="email">
             <FormLabel>Email address</FormLabel>
-            <Input width={300} type="email" placeholder="john@mail.com" size="md" />
+            <Input
+              width={300} 
+              type="email" 
+              placeholder="john@mail.com" 
+              size="md"
+              onChange={e => setEmail(e.target.value)}
+            />
           </FormControl>
           <FormControl id="password">
             <FormLabel>Password</FormLabel>
-            <Input width={300} type="password" placeholder="Enter your password" size="md" />
+            <Input 
+              width={300} 
+              type="password" 
+              placeholder="Enter your password" 
+              size="md"
+              onChange={e => setPassword(e.target.value)}
+            />
           </FormControl>
-          <Button size="md" width={300} colorScheme="blue">
+          <Button 
+            onClick={submitForm} 
+            size="md" width={300}
+            colorScheme="blue"
+          >
             Log in
           </Button>
         </Stack>
