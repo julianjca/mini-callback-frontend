4b9e93f4c89fcf0459691260e43f0dc68601156d
Julian Anderson <julian@jumpcut.com>
2021-02-18 17:12:10 +0700

    adding sign up and login form

---
+++ src/components/Login/index.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Box, Center, Container, Heading, Stack, Input, FormControl, FormLabel, Button } from "@chakra-ui/react"
+
+const LogIn = () => {
+  return (
+    <Container>
+      <Center>
+        <Heading>
+          Welcome
+        </Heading>
+      </Center>
+      <Center mt={10}>
+        <Stack spacing={5}>
+          <FormControl id="email">
+            <FormLabel>Email address</FormLabel>
+            <Input width={300} type="email" isFullWidth placeholder="john@mail.com" size="md" />
+          </FormControl>
+          <FormControl id="email">
+            <FormLabel>Password</FormLabel>
+            <Input width={300} type="password" isFullWidth placeholder="Enter your password" size="md" />
+          </FormControl>
+          <Button size="md" width={300} colorScheme="blue">
+            Log in
+          </Button>
+        </Stack>
+      </Center>
+    </Container>
+  )
+}
+
+export default LogIn
---
+++ src/pages/index.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import Head from 'next/head'
+import { Box, Center, Text, Link } from "@chakra-ui/react"
+
+import LogIn from '../components/Login'
+import Register from '../components/Register'
+
+export default function Home() {
+  const [renderRegisterPage, setRenderRegisterPage] = React.useState(false)
+  return (
+    <Box pt={20} pb={20}>
+      <Head>
+        <title>Callback UI</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      {renderRegisterPage ? (
+        <>
+          <Register />
+          <Center mt={10}>
+            <Text>
+             Have an account?{" "}
+              <Link onClick={() => setRenderRegisterPage(false)} colorScheme="blue" color="blue">
+                login instead.
+              </Link>
+            </Text>
+          </Center>
+        </>
+      ) : (
+        <>
+          <LogIn />
+          <Center mt={10}>
+            <Text>
+              Don't have an account yet?{" "}
+              <Link onClick={() => setRenderRegisterPage(true)} colorScheme="blue" color="blue">
+                create one
+              </Link>
+            </Text>
+          </Center>
+        </>
+      )}
+      
+    </Box>
+  )
+}
